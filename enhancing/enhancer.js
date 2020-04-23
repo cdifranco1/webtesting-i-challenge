1fc5faae2681ce45b38b7fe3a39fac50c5e93a83
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-04-22 20:55:55 -0500

    completed mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,58 @@ module.exports = {
   get,
 };
 
+const item = {
+  name: 'sword',
+  durability: 0,
+  enhancement: 0
+}
+
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20){
+    return {
+      ...item,
+      enhancement: item.enhancement + 1
+    }
+  }
+  return item
 }
 
 function fail(item) {
-  return { ...item };
+  const { durability, enhancement } = item
+  let newDurability = durability
+  let newEnhancement = enhancement
+
+  if (enhancement < 15 && enhancement >= 5){
+    newDurability = durability - 5
+  }
+  
+  if (enhancement >= 15 && durability < 10){
+    newDurability = 0
+  } else if (enhancement >= 15){
+    newDurability = durability - 10
+  }
+  
+  if (enhancement > 16){
+    newEnhancement = enhancement - 1
+  }
+
+  if (durability < 5){
+    newDurability = 0
+  }
+
+  return {
+    ...item, 
+    durability: newDurability,
+    enhancement: newEnhancement
+  }
 }
 
+
 function repair(item) {
-  return { ...item };
+  return { 
+    ...item, 
+    durability: 100
+  };
 }
 
 function get(item) {
